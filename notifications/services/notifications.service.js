7a44e67f4658a1c4696cdae3dc8cd062717fb5bb
Dayanna Vanessa Suarez M <dayanna.suarez@uao.edu.co>
2023-03-22 17:11:26 -0500

    Añadir comentarios a notification service

--- notifications/services/notifications.service.js
+++ notifications/services/notifications.service.js
@@ -7,21 +7,31 @@ class NotificationsService {
   constructor() {}
 
   async findAll() {
+    //Traer todas las notificaciones
     let notifications = await models.Notification.findAll();
-    const taskIds = notifications.map((notification) => notification.taskId);
+    // Obtener todos los taskIDs de cada notificacion
+    const taskIds = notifications.map((notification) => notification.taskId); // [1,2,3,4,7]
 
+    //Consultar las tareas por el arreglo de tasksIDs
     const { data: tasks } = await axios.post(`${TASKS_SERVICE_URL}/idbatch`, {
       ids: taskIds,
-    });
+    }); //Devuelve un arreglo de tareas
 
     return await Promise.all(
+      //Recorrer las notificaciones
       notifications.map(async (notification) => {
+        //Buscar tarea asociada a la notificacion
         const task = tasks.find((task) => notification.taskId === task.id);
+        //Agregar desripcion de tarea a la notificacion
         notification.dataValues.task = task.description;
+
+        //Consultar usuario por id a servicio de usuarios
         const { data: user } = await axios.get(
           USERS_SERVICE_URL + '/' + notification.userId
-        );
+        ); // Devuelve un usuario por su id
+        //Agregar nomre de usuario a notificacion
         notification.dataValues.user = user.name;
+
         return notification;
       })
     );
